d2cb0e95ed89b39e3c34420c8777e71b46f32cf2
gleb-skobinsky <glebgytnik@gmail.com>
2023-06-11 01:06:54 +0300

    Initial commit

--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -2,16 +2,14 @@ sealed class PersonImpl {
     abstract val name: String?
     abstract val surName: String?
 
-    abstract fun copySealed(name: String? = this.name, surName: String? = this.surName): PersonImpl
+    abstract fun copyS(name: String? = this.name, surName: String? = this.surName): PersonImpl
 
     data class Employee(
         override val name: String?,
         override val surName: String?,
         val age: Int,
     ) : PersonImpl() {
-        override fun copySealed(name: String?, surName: String?): PersonImpl {
-            return copy(name = name, surName = surName)
-        }
+        override fun copyS(name: String?, surName: String?): PersonImpl = copy(name = name, surName = surName)
     }
 
     data class Client(
@@ -19,9 +17,7 @@ sealed class PersonImpl {
         override val surName: String?,
         val gender: Gender,
     ) : PersonImpl() {
-        override fun copySealed(name: String?, surName: String?): PersonImpl {
-            return copy(name = name, surName = surName)
-        }
+        override fun copyS(name: String?, surName: String?): PersonImpl = copy(name = name, surName = surName)
     }
 }
 
